b23d0e6bdef9a508aacdda70e2d71661b99af919
Thomas <tom@axisj.com>
2019-03-13 19:47:26 +0900

    ...

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import 'styles/globals';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import { hot } from 'react-hot-loader';
+import { hot } from 'react-hot-loader/root';
 import { NoMatch, ErrorBoundary } from 'components';
 import 'styles/globals';
 import Samples from './Sample';
 
-class App extends React.Component {
+class App extends React.Component<any> {
   render() {
     return (
       <ErrorBoundary>
